3d1074b3ff3f2340a492dc14415e04a445e39e59
Joel Chubrega <jchubrega@gmail.com>
2025-10-10 13:06:59 -0300

    Router create

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import productsRouter from "./routes/productsRouter.js";
+import cartRouter from "./routes/cartRouter.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+
+app.use("api/products", productsRouter);
+app.use("api/carts", cartRouter);
 
 app.use((req, res) => {
   res.status(404).json({ error: "Ruta no encontrada" });
---
+++ routes/cartRouter.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import CartManager from "../managers/cartManager.js";
+
+const router = Router();
+const cartManager = new CartManager();
+
+router.post("/", async (req, res) => {
+  try {
+    const cart = await cartManager.createCart();
+    res.status(201).json(cart);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/:cid", async (req, res) => {
+  try {
+    const cart = await cartManager.getCartById(req.params.cid);
+    if (!cart) {
+      return res.status(404).json({ error: "Carrito no encontrado" });
+    }
+    res.json(cart.products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/:cid/product/:pid", async (req, res) => {
+  try {
+    const cart = await cartManager.addProductToCart(
+      req.params.cid,
+      req.params.pid
+    );
+    if (!cart) {
+      return res.status(404).json({ error: "Carrito no encontrado" });
+    }
+    res.json(cart);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
