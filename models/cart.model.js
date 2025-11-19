d07e0995dfdd27a9c04fab0930f280efc7a498a3
Joel Chubrega <jchubrega@gmail.com>
2025-11-19 02:03:18 -0300

    add MongoDB & models

---
+++ models/cart.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: { type: Number, required: true, default: 1 },
+  },
+  { _id: false }
+);
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+export default Cart;
---
+++ models/product.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+
+const productSchema = new mongoose.Schema(
+  {
+    title: { type: String, unique: true },
+    description: String,
+    price: Number,
+    stock: Number,
+    status: { type: Boolean, default: true },
+    category: String,
+    thumbnails: { type: [String], default: [] },
+  },
+  { timestamps: true }
+);
+
+productSchema.plugin(mongoosePaginate);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
